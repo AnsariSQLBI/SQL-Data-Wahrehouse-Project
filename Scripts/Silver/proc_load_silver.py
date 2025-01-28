699a6d814951953b7eafc02d4b9f6f70ebdb969a
Aboozar Ansari <ansari.aboozar@gmail.com>
2025-01-28 19:34:04 +0330

    Update proc_load_silver.py

--- Scripts/Silver/proc_load_silver.py
+++ Scripts/Silver/proc_load_silver.py
@@ -9,9 +9,6 @@ df['cst_marital_status'] = df['cst_marital_status'].str.strip().str.upper().repl
     {'S': 'Single', 'M': 'Married', 'NULL': 'n/a'})
 df['cst_gndr'] = df['cst_gndr'].str.strip().str.upper().replace(
     {'M': 'Male', 'F': 'Female', 'NULL': 'n/a'})
-print(df['cst_id'].value_counts())
-
 filepath = Path('Datasets/source_crm/silver_cust_info.csv')
 filepath.parent.mkdir(parents=True, exist_ok=True)
 df.to_csv(filepath)
-
